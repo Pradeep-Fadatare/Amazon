f7fdb26bddfe9a52d654b88714317efca5c78005
Pradeep Fadatare <padyafadatare@gmail.com>
2022-10-22 17:19:16 +0530

    This Commit From USA

--- src/main/java/GitCheck.java
+++ src/main/java/GitCheck.java
@@ -1,6 +1,12 @@
 public class GitCheck {
 
     public static void main(String[] args) {
+        GitCheck gitCheck=new GitCheck();
+        gitCheck.checkFromUsa();
         System.out.println("Check whether this is visible to AmazonClonedFromGit");
+    }
+
+    public void checkFromUsa(){
+        System.out.println("Hellow India I am Visible ???");
     }
 }
